f80caeae949a2f12b5b5266da875450870f46f22
Warren Buckley <warren@umbraco.com>
2022-05-05 17:08:55 +0100

    Start wiring up the default parts needed to be a form element
    https://web.dev/more-capable-form-controls/

--- src/two-factor-element.ts
+++ src/two-factor-element.ts
@@ -7,7 +7,30 @@ import { styles } from './styles';
  */
 @customElement('two-factor')
 export class TwoFactorElement extends LitElement {
+
+  /**
+   * CSS Styles that are stored in styles.ts to keep code a little neater
+   */
   static styles = styles;
+
+  /**
+   * This is a static class field indicating that the element can be used inside a native form and participate in its events.
+   * Read more about form controls here https://web.dev/more-capable-form-controls/
+   */
+  static readonly formAssociated = true;
+
+  /**
+   * The constructor of this class & WebComponent
+   * Will ensure it assigns the Form Element Internals API for us to consume
+   */
+     constructor() {
+      super();
+      this.value = '';
+      this._internals = this.attachInternals();
+    }
+  
+    @property({ reflect: false })
+    value;
 
   /**
    * The length of the two factor auth code
@@ -15,6 +38,27 @@ export class TwoFactorElement extends LitElement {
    */
   @property({ type: Number, attribute: 'length' })
   codeLength = 6;
+
+  /**
+   * This stores the Form ElementInternals API
+   * https://developer.mozilla.org/en-US/docs/Web/API/ElementInternals
+   */
+  private _internals:any;
+
+  // Mostly boilerplate--add common form control properties and methods
+  // Many are simply wired through to the ElementInternals object.
+  get form() { return this._internals.form; }
+  get name() { return this.getAttribute('name')};
+  get type() { return this.localName; }
+
+  get validity() { return this._internals.validity; }
+  get validationMessage() { return this._internals.validationMessage; }
+  get willValidate() { return this._internals.willValidate; }
+
+  checkValidity() { return this._internals.checkValidity(); }
+  reportValidity() { return this._internals.reportValidity(); }
+  
+
 
   /**
    * This creates the HTML inside the WebComponent Shadow DOM
