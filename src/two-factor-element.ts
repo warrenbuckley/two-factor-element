856bef0f207e972013670e1195b74b6fd82aa2b0
Warren Buckley <warren@umbraco.com>
2022-05-05 16:51:50 +0100

    Remove unused CSS

--- src/two-factor-element.ts
+++ src/two-factor-element.ts
@@ -1,4 +1,4 @@
-import { html, css, LitElement } from 'lit';
+import { html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { styles } from './styles';
 
